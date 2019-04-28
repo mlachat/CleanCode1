9f5d5428de55824dea0d07a4005f9315d6ea1e9d
Markus Lachat <ml@markus-lachat.com>
2019-04-28 21:28:05 +0200

    yes we need java 8 ;)

--- src/main/java/de/lv1871/cleancode/Foo.java
+++ src/main/java/de/lv1871/cleancode/Foo.java
@@ -1,8 +1,19 @@
 package de.lv1871.cleancode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Foo {
 
     public String greeting() {
+        List<String> list2Stream = new ArrayList<>();
+        list2Stream.add("2");
+        list2Stream.add("3");
+        List<Integer> collectionOfTwosOnly = list2Stream.stream()
+                .map(a -> a.compareTo("2"))
+                .collect(Collectors.toList());
+
         return "Hello World";
     }
 }
